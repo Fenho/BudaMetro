69616bbef98aee66379679dc760768ec52b58825
unknown <foarriagada@uc.cl>
2022-05-15 13:13:43 -0500

    Added 3 tests for robustness :rice_ball:

--- test_shortest.py
+++ test_shortest.py
@@ -3,37 +3,63 @@ import unittest
 from main import make_network, get_shortest
 from io_handler import network_processer
 
+def test_helper(network_descriptor, start, end, color):
+    nodes = network_processer(network_descriptor)
+    network = make_network(nodes, color)
+    return get_shortest(network, start, end)
+
 class TestShortest(unittest.TestCase):
-    def test_shortest(self):
+    def test_task_example_no_color(self):
         network_descriptor = "./Networks/first_example.csv"
-        nodes = network_processer(network_descriptor)
         start, end = "A", "F"
         color = ""
-
-        network = make_network(nodes, color)
-        visited_str = get_shortest(network, start, end)
+        visited_str = test_helper(network_descriptor, start, end, color)
         self.assertEqual(visited_str, "A->B->C->D->E->F")
 
-    def test_shortest_red(self):
+    def test_task_example_red(self):
         network_descriptor = "./Networks/first_example.csv"
-        nodes = network_processer(network_descriptor)
         start, end = "A", "F"
         color = "red"
-
-        network = make_network(nodes, color)
-        visited_str = get_shortest(network, start, end)
+        visited_str = test_helper(network_descriptor, start, end, color)
         self.assertEqual(visited_str, "A->B->C->H->F")
 
-    def test_shortest_green(self):
+    def test_task_example_green(self):
         network_descriptor = "./Networks/first_example.csv"
-        nodes = network_processer(network_descriptor)
         start, end = "A", "F"
         color = "green"
-
-        network = make_network(nodes, color)
-        visited_str = get_shortest(network, start, end)
+        visited_str = test_helper(network_descriptor, start, end, color)
         posible_answers = ["A->B->C->D->E->F", "A->B->C->G->I->F"]
         self.assertIn(visited_str, posible_answers)
+
+    def test_second_middle(self):
+        network_descriptor = "./Networks/second_example.csv"
+        start, end = "A", "I"
+        color = ""
+        visited_str = test_helper(network_descriptor, start, end, color)
+        posible_answers = ["A->B->C->F->I", "A->B->G->H->I"]
+        self.assertIn(visited_str, posible_answers)
+
+    def test_second_three_connections(self):
+        network_descriptor = "./Networks/second_example.csv"
+        start, end = "A", "E"
+        color = "green"
+        visited_str = test_helper(network_descriptor, start, end, color)
+        posible_answers = ["A->B->D->E", "A->B->F->E"]
+        self.assertIn(visited_str, posible_answers)
+
+    def test_second_three_connections(self):
+        network_descriptor = "./Networks/second_example.csv"
+        start, end = "A", "I"
+        color = "green"
+        visited_str = test_helper(network_descriptor, start, end, color)
+        self.assertEqual(visited_str, "A->B->F->I")
+
+    def test_second_red(self):
+        network_descriptor = "./Networks/second_example.csv"
+        start, end = "A", "I"
+        color = "red"
+        visited_str = test_helper(network_descriptor, start, end, color)
+        self.assertEqual(visited_str, "A->C->I")
 
 if __name__ == '__main__':
     unittest.main()
